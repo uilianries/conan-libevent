579819d38e8090f6ee1a962894e0cd2c84d1a3f8
Jerry W <jerrywiltse@gmail.com>
2018-03-02 08:50:15 -0500

    [skip ci]another try at fixing findlibevent, still doesn't work

--- FindLibEvent.cmake
+++ FindLibEvent.cmake
@@ -1,16 +1,28 @@
 
 message(STATUS "${CONAN_LIB_DIRS_LIBEVENT}")
 
-find_path(LIBEVENT_INCLUDE_DIR NAMES LIBEVENT.h PATHS ${CONAN_INCLUDE_DIRS_LIBEVENT})
-find_library(LIBEVENT_LIBRARY NAMES ${CONAN_LIBS_LIBEVENT} PATHS ${CONAN_LIB_DIRS_LIBEVENT})
+find_path(LIBEVENT_INCLUDE_DIR event.h PATHS ${CONAN_INCLUDE_DIRS_LIBEVENT})
+find_library(LIBEVENT_LIB NAMES event PATHS ${CONAN_LIB_DIRS_LIBEVENT})
 
-set(LIBEVENT_INCLUDE_DIRS ${LIBEVENT_INCLUDE_DIR})
-set(LIBEVENT_LIBRARIES ${LIBEVENT_LIBRARY} ${EXTRA_LIBS})
+if (LIBEVENT_LIB AND LIBEVENT_INCLUDE_DIR)
+  set(LibEvent_FOUND TRUE)
+  set(LIBEVENT_LIB ${LIBEVENT_LIB})
+else ()
+  set(LibEvent_FOUND FALSE)
+endif ()
 
-mark_as_advanced(LIBEVENT_LIBRARY LIBEVENT_INCLUDE_DIR)
+if (LibEvent_FOUND)
+  if (NOT LibEvent_FIND_QUIETLY)
+    message(STATUS "Found libevent: ${LIBEVENT_LIB}")
+  endif ()
+else ()
+  if (LibEvent_FIND_REQUIRED)
+    message(FATAL_ERROR "Could NOT find libevent.")
+  endif ()
+  message(STATUS "libevent NOT found.")
+endif ()
 
-message("** LIBEVENT found by Conan!")
-set(LIBEVENT_FOUND TRUE)
-message("   - libraries:  ${LIBEVENT_LIBRARIES}")
-message("   - includes:  ${LIBEVENT_INCLUDE_DIRS}")
-
+mark_as_advanced(
+    LIBEVENT_LIB
+    LIBEVENT_INCLUDE_DIR
+  )
