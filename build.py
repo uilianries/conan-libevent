5c3e6ffe85ac52eb95225b12899747981258a51e
Uilian Ries <uilianries@gmail.com>
2018-11-08 19:29:19 -0200

    Build with OpenSSl on Windows
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,36 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 from bincrafters import build_template_default
 import platform
-import copy
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(pure_c=True)
-
-    # libevent 2.0 doesn't support shared build on Windows
-    # so remove those builds that have shared=True
-    # and remove windows shared builds at all by now
-    items = []
-    for item in builder.items:
-        if platform.system() == "Windows":
-            #and item.reference.version.startswith('2.0.'):
-            if not item.options.get('libevent:shared', False):
-                items.append(item)
-        else:
-            items.append(item)
-    builder.items = items
-
-    # Add Windows builds without OpenSSL too
+    shared_option_name = False if platform.system() == "Windows" else "libevent:shared"
+    builder = build_template_default.get_builder(pure_c=True, shared_option_name=shared_option_name)
     if platform.system() == "Windows":
-        items = []
-        for item in builder.items:
-            new_options = copy.copy(item.options)
-            new_options["libevent:with_openssl"] = False
-            items.append([item.settings, new_options, item.env_vars,
-                item.build_requires, item.reference])
-            builder.items = items
-
+        for settings, options, env_vars, build_requires, reference in reversed(builder.items):
+            builder.add(settings, {"libevent:with_openssl": False}, env_vars, build_requires)
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,7 @@ class LibeventConan(ConanFile):
     url = "https://github.com/bincrafters/conan-libevent"
     homepage = "https://github.com/libevent/libevent"
     author = "Bincrafters <bincrafters@gmail.com>"
-    license = "BSD 3-Clause"
+    license = "BSD-3-Clause"
     exports = ["LICENSE.md"]
     exports_sources = ["print-winsock-errors.c"]
     source_subfolder = "source_subfolder"
@@ -28,40 +28,30 @@ class LibeventConan(ConanFile):
                        "disable_threads": False}
 
     @property
-    def is_v21(self):
-        return self.version.startswith('2.1.')
-
-    @property
-    def is_v20(self):
-        return self.version.startswith('2.0.')
+    def _is_shared(self):
+        return self.settings.os != "Windows" and self.options.shared
 
     def config_options(self):
         if self.settings.os == "Windows":
-            self.options.remove("fPIC")
-        # 2.0 cannot do openssl on Windows
-        if self.settings.os == "Windows" and self.is_v20:
-            self.options.with_openssl = False
+            del self.options.fPIC
+            del self.options.shared
 
     def configure(self):
         del self.settings.compiler.libcxx
-        if self.settings.os == "Windows" and self.options.shared:
-            raise ConanInvalidConfiguration("Shared builds on Windows are not supported")
 
     def requirements(self):
         if self.options.with_openssl:
             self.requires.add("OpenSSL/1.0.2o@conan/stable")
-            if self.options.shared:
+            if self._is_shared:
                 # static OpenSSL cannot be properly detected because libevent picks up system ssl first
                 # so enforce shared openssl
                 self.output.warn("Enforce shared OpenSSL for shared build")
-                self.options["OpenSSL"].shared = self.options.shared
+                self.options["OpenSSL"].shared = self._is_shared
 
     def source(self):
         tools.get("{0}/releases/download/release-{1}-stable/libevent-{1}-stable.tar.gz".format(self.homepage, self.version))
         os.rename("libevent-{0}-stable".format(self.version), self.source_subfolder)
-        if self.is_v21:
-            # copy missing test source, https://github.com/libevent/libevent/issues/523
-            shutil.copy("print-winsock-errors.c", os.path.join(self.source_subfolder, "test"))
+        shutil.copy("print-winsock-errors.c", os.path.join(self.source_subfolder, "test"))
 
     def build(self):
 
@@ -74,7 +64,7 @@ class LibeventConan(ConanFile):
             # 1. set *LD_LIBRARY_PATH (works with Linux with RunEnvironment but does not work on OS X 10.11 with SIP)
             # 2. copying dylib's to the build directory (fortunately works on OS X)
             imported_libs = []
-            if self.options.shared and self.settings.os == "Macos":
+            if self._is_shared and self.settings.os == "Macos":
                 for dep in self.deps_cpp_info.deps:
                     for libname in os.listdir(self.deps_cpp_info[dep].lib_paths[0]):
                         if libname.endswith('.dylib'):
@@ -91,7 +81,7 @@ class LibeventConan(ConanFile):
 
             # compose configure options
             suffix = ''
-            if not self.options.shared:
+            if not self._is_shared:
                 suffix += " --disable-shared "
             if self.options.with_openssl:
                 suffix += "--enable-openssl "
@@ -120,7 +110,7 @@ class LibeventConan(ConanFile):
         elif self.settings.os == "Windows":
             vcvars = tools.vcvars_command(self.settings)
             suffix = ''
-            if self.is_v21 and self.options.with_openssl:
+            if self.options.with_openssl:
                 suffix = "OPENSSL_DIR=" + self.deps_cpp_info['OpenSSL'].rootpath
             # add runtime directives to runtime-unaware nmakefile
             tools.replace_in_file(os.path.join(self.source_subfolder, "Makefile.nmake"),
@@ -137,16 +127,12 @@ class LibeventConan(ConanFile):
         self.copy("LICENSE", src=self.source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
         self.copy("*.h", dst="include", src=os.path.join(self.source_subfolder, "include"))
         if self.settings.os == "Windows":
-            if self.is_v21:
-                self.copy("event-config.h", src=os.path.join(self.source_subfolder, "WIN32-Code", "nmake", "event2"), dst="include/event2")
-            else:
-                # Windows build is not using configure, so event-config.h is copied from WIN32-Code folder
-                self.copy("event-config.h", src=os.path.join(self.source_subfolder, "WIN32-Code", "event2"), dst="include/event2")
+            self.copy("event-config.h", src=os.path.join(self.source_subfolder, "WIN32-Code", "nmake", "event2"), dst="include/event2")
             self.copy("tree.h", src=os.path.join(self.source_subfolder, "WIN32-Code"), dst="include")
             self.copy(pattern="*.lib", dst="lib", keep_path=False)
         for header in ['evdns', 'event', 'evhttp', 'evrpc', 'evutil']:
             self.copy(header+'.h', dst="include", src=self.source_subfolder)
-        if self.options.shared:
+        if self._is_shared:
             if self.settings.os == "Macos":
                 self.copy(pattern="*.dylib", dst="lib", keep_path=False)
             else:
@@ -161,5 +147,5 @@ class LibeventConan(ConanFile):
 
         if self.settings.os == "Windows":
             self.cpp_info.libs.append('ws2_32')
-            if self.is_v21 and self.options.with_openssl:
+            if self.options.with_openssl:
                 self.cpp_info.defines.append('EVENT__HAVE_OPENSSL=1')
